daafe96adc0de3c6a6b835ce96477efcedb9e3f9
Lukas <lukas.hauser@me.com>
2021-07-04 11:33:08 +0100

    update blogpost views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,19 @@
 module ApplicationHelper
   def full_title(page_title = ' ')
-  base_title = 'Lukas Hauser'
-  if page_title.empty?
-    base_title
-  else
-    page_title + ' | ' + base_title
+    base_title = 'Lukas Hauser'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
   end
-end
+
+  def background_image(image_url)
+    base_url = asset_path('thunderstruck.jpg')
+    if image_url.empty?
+      base_url
+    else
+      image_url
+    end
+  end
 end
